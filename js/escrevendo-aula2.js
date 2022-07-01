469f454d85addcb46931e2b8a3e6456a08e78bb8
Edu Vimrond <eduvirmond@me.com>
2022-07-01 08:53:25 -0300

    Vader to the win

--- js/escrevendo-aula2.js
+++ js/escrevendo-aula2.js
@@ -3,7 +3,7 @@ var button = document.querySelector("button");
 button.onclick = function () {
   var nome = prompt("Qual é o comando para propor o repositorio remoto o git?");
   if ((nome == "git remote")) {
-    alert("O " + nome + ", serve para iniciar o git. Parabéns você acertou!");
+    alert("O " + nome + ", serve para iniciar o repositorio remoto. Parabéns você acertou!");
   }
   else
   {
--- js/escrevendo-aula7.js
+++ js/escrevendo-aula7.js
@@ -1,13 +1,13 @@
 var button = document.querySelector("button");
 
 button.onclick = function () {
-  var nome = prompt("Qual é o comando para ramificar o git?");
-  if ((nome == "git branch")) {
+  var nome = prompt("Qual é o comando para listar os documentos da pasta?");
+  if ((nome == "ls")) {
     alert("O " + nome + ", serve para iniciar o git. Parabéns você acertou!");
   }
   else
   {
-    alert("O" + nome +" nao serve! ")
+    alert("O " + nome +" nao serve! ")
   }
-  window.location="aula7.html";
+  window.location="aula8.html";
 };
